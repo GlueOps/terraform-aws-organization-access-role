b7da90f1624b9b4f0ae76b33641f63f817c6c640
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-09-21 16:23:09 -0700

    feat: remove aws.management

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 2.7.0"
-      configuration_aliases = [ aws.management_account ]
     }
   }
 }
@@ -11,7 +10,6 @@ terraform {
 
 # https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
 data "aws_iam_policy_document" "assume_role" {
-  provider = aws.management_account
   statement {
     actions = [
       "sts:AssumeRole",
@@ -28,7 +26,6 @@ data "aws_iam_policy_document" "assume_role" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_role.html
 resource "aws_iam_role" "default" {
-  provider = aws.management_account
   name               = var.role_name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   description        = "The role to grant permissions to this account to delegated IAM users in the master account"
@@ -37,7 +34,6 @@ resource "aws_iam_role" "default" {
 # https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
 # By default it attaches `AdministratorAccess` managed policy to grant full access to AWS services and resources in the current account
 resource "aws_iam_role_policy_attachment" "default" {
-  provider = aws.management_account
   role       = aws_iam_role.default.name
   policy_arn = var.policy_arn
 }
